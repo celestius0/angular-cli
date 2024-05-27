aaf2c5dad1759cef9252de73c4906d8bdab1ae99
Ash Ramirez <ash@neonexus.co>
2024-05-26 20:41:28 -0700

    Update all license file links

--- packages/angular_devkit/build_angular/src/builders/jest/init-test-bed.mjs
+++ packages/angular_devkit/build_angular/src/builders/jest/init-test-bed.mjs
@@ -3,7 +3,7 @@
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 
 // TODO(dgp1130): These imports likely don't resolve in stricter package environments like `pnpm`, since they are resolved relative to
